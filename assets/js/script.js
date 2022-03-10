a99695b617a5b483fd709be05f0e4a0555a7d0be
noreikam <noreikam1@gmail.com>
2022-03-09 23:19:30 -0500

    added favorites feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@
 var searchBtn = document.getElementById('searchBtn');
 var searchBox = document.getElementById('searchText');
 var localBtn = document.getElementById('localButton');
-var randomBox = document.getElementById('randomBox');
+var restList = document.getElementById('restList');
 var citySelect = document.getElementById('citySelect');;
 var restCard = document.getElementById('restCard');
+var addFavoriteBtn = document.getElementById('addFavorite');
+var favoriteBtn = document.getElementById('favoriteBtn');
+var indexDisplayed;
+var favIndex = 0;
 
 function getLocationId(event) {
     event.preventDefault();
@@ -17,7 +21,7 @@ function getLocationId(event) {
     getRandomRest(locationId);
 }
 
-// function to fetch restaurant data from Worldwide Restaurants API hartford=33804 westHartford=33970
+// function to fetch restaurant data from Worldwide Restaurants API 
 function getRandomRest(locationId) {
     fetch("https://worldwide-restaurants.p.rapidapi.com/search?currency=USD&language=en_US&location_id=" + locationId + "&limit=1000", 
     {
@@ -65,7 +69,7 @@ function getRandomRest(locationId) {
 
   //function to display random restaurants 
   var displayRandRest = function() {
-    randomBox.innerHTML = "<div class='px-5 py-8 border-2 border-green-800'><h2 class='font-bold'>Local Restaurants</h2>";
+    restList.innerHTML = "<div class='px-5 py-8 border-2 border-green-800'><h2 class='font-bold'>Local Restaurants</h2>";
     var randRestArr = JSON.parse(localStorage.getItem("randRestArr"));
     var maxDisplay = 5;
     if(randRestArr.length<maxDisplay) {
@@ -89,24 +93,25 @@ function getRandomRest(locationId) {
         var restSnippet = document.createElement('p');
 
         // append to random box
-        randomBox.appendChild(restSnippet);
+        restList.appendChild(restSnippet);
 
         // add content to HTML 
         restSnippet.outerHTML = "<p class='py-2 px-4 border-b-2 border-green-800'>" + restName + "</p>"; 
     }
-    displayRestCard(randIndexArr[0]);    
+    displayRestCard("randRestArr", randIndexArr[0]);    
   }
 
 // function to display restaurant details in page's hero section
-var displayRestCard = function(index) {
+var displayRestCard = function(arrayName, index) {
     
     console.log("displayRestCard()");
     console.log(index);
-    console.log(restObj);
+    indexDisplayed = index;
 
     // choose a restaurant
-    var randRestArr = JSON.parse(localStorage.getItem("randRestArr"));
-    var restObj = randRestArr[index];
+    var restArr = JSON.parse(localStorage.getItem(arrayName));
+    var restObj = restArr[index];
+    console.log(restObj);
 
     // assign restaurant details to local variables
     var imageURL = 'http://www.ncenet.com/wp-content/uploads/2020/04/No-image-found.jpg';
@@ -128,10 +133,67 @@ var displayRestCard = function(index) {
     if (restObj.awards.length > 0) {
         award = restObj.awards[0].display_name;
     }
-
+    // use innerHTML to display restaurant details
     restCard.innerHTML = 
-        "<div class='flex'><div class='flex-column items-center justify-center flex-wrap m-auto'><h1 class='font-extrabold py-1.5'>" + restObj.name + "</h1><img class='w-full border-4 border-green-800' src='" + imageURL + "'</img><h3 class='font-semibold py-1.5'>Cuisine: " + cuisine + "</h3></div><div class='text-lg text-center py-1.5 px-1.5'><h3>" + restObj.street + "</h3><h3>" + restObj.city + ", " + restObj.state + "</h3><div class='py-1.5'><a class='text-green-700 visited:text-green-400 hover:text-green-900 underline' href='" + restObj.website_url + "'>Restaurant Website</a></div><div><a href='" + restObj.trip_advisor_url + "'>Reviews</a></div><h3>Price Range: " + priceRange + "</h3><h3>Rating: " + rating + "/4</h3><h3>" + award + "</h3></div></div></div><div><p>" + restObj.description + "</p></div>"
+        "<div class='flex'><div class='flex-column items-center justify-center flex-wrap m-auto'><h1 class='font-extrabold py-1.5'>" + restObj.name + "</h1><img class='w-full border-4 border-green-800' src='" + imageURL + "'</img><h3 class='font-semibold py-1.5'>Cuisine: " + cuisine + "</h3></div><div class='text-lg text-center py-1.5 px-1.5'><h3>" + restObj.street + "</h3><h3>" + restObj.city + ", " + restObj.state + "</h3><div class='py-1.5'><a class='text-green-700 visited:text-green-400 hover:text-green-900 underline' href='" + restObj.website_url + "'>Restaurant Website</a></div><div><a href='" + restObj.trip_advisor_url + "'>Reviews</a></div><h3>Price Range: " + priceRange + "</h3><h3>Rating: " + rating + "/4</h3><h3>" + award + "</h3><button id='addFavorite' class = 'box-shadow py-1 px-4 my-4 bg-green-300 rounded-full' type='submit'>Add Favorite</button></div></div></div><div><p>" + restObj.description + "</p></div>";
+
+        addFavoriteBtn = document.getElementById('addFavorite');
+        addFavoriteBtn.addEventListener('click', addFavorite);
 }
+
+// add currently displayed restaurant to local storage favorites array
+var addFavorite = function() {
+    console.log("Add Favorite");
+    var randRestArr = JSON.parse(localStorage.getItem("randRestArr"));
+    var restObj = randRestArr[indexDisplayed];
+    console.log("restObj from addFavorite");
+    console.log(restObj);
+    var favRestArr;
+    if(JSON.stringify(localStorage.favRestArr)){
+        console.log("exists");
+        favRestArr = JSON.parse(localStorage.favRestArr);
+    }
+    else{ 
+        console.log("does not exist");
+        favRestArr = [];
+    }
+    console.log(favRestArr);
+    favRestArr.push(restObj);
+    console.log(favRestArr);
+
+    localStorage.setItem("favRestArr", JSON.stringify(favRestArr));
+
+}
+
+// display the favorites modal
+var displayFavorites = function() {
+    console.log("clicked favorites");
+
+    // change heading to favorites
+    restList.innerHTML = "<div class='px-5 py-8 border-2 border-green-800'><h2 class='font-bold'>Favorite Restaurants</h2>";
+    // retrieve favorites from local storage
+    var favRestArr = JSON.parse(localStorage.getItem("favRestArr"));
+    
+    // loop over favorites array and display each
+    for (var i=0; i<favRestArr.length; i++){
+        // retrieve restaurant data
+        var restName = favRestArr[i].name;
+        
+        // create empty HTML 
+        var restSnippet = document.createElement('p');
+
+        // append to restaurant list box
+        restList.appendChild(restSnippet);
+
+        // add content to HTML 
+        restSnippet.outerHTML = "<p id='restBtn' class=' py-2 px-4 border-b-2 border-green-800' type='submit'>" + restName + "</p>"; 
+    }
+    var restBtn = document.getElementById("restBtn");
+    restBtn.addEventListener("click", displayRestCard("favRestArr", 0));
+
+    
+}
+    
 
 
 // this array of objects allows the City dropdown to be populated and maps the selected text to a locationId that the Worldwide Restaurant API needs to use
@@ -188,9 +250,11 @@ window.addEventListener("load", () => {
         var cityOption = document.createElement("option");
         cityOption.textContent = cityArr[i].cityText;
         citySelect.appendChild(cityOption);
-  }});
+  }
+  getRandomRest(33804);
+});
 
 
-//citySelect.addEventListener('change', getLocationId);
 searchBtn.addEventListener('click', getLocationId);
+favoriteBtn.addEventListener('click',displayFavorites);
 
